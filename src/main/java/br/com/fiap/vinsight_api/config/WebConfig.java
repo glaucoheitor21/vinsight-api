a773e2163eb3a9a1b10b769387ae789adc930dd1
glaucoheitor21 <talonelygod@gmail.com>
2026-05-19 10:51:28 -0300

    Ativando PageSerializationMode.VIA_DTO para compatibilidade futura (?)

---
+++ src/main/java/br/com/fiap/vinsight_api/config/WebConfig.java
@@ -0,0 +1,10 @@
+package br.com.fiap.vinsight_api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode;
+
+@Configuration
+@EnableSpringDataWebSupport(pageSerializationMode = PageSerializationMode.VIA_DTO)
+public class WebConfig {
+}
